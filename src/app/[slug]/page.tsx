8777da885a2215ac5b29c29d98635b0f9c6c6f6d
TeeGreat <toyinatofarati64@gmail.com>
2024-06-09 11:54:42 +0100

    product single page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
+import React from "react";
 
 const SinglePage = () => {
   return (
-    <div>SinglePage</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:64 relative flex flex-col lg:flex-row gap-16">
+      {/* Image */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* text */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione illo
+          hic cupiditate nemo ut exercitationem asperiores, culpa architecto
+          inventore veritatis?
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab
+            delectus sapiente facilis. Asperiores error consequatur
+            reprehenderit quisquam quas, dolor laboriosam rem, nisi, ut ipsam
+            eius aspernatur voluptas accusantium quasi? Dolores.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab
+            delectus sapiente facilis. Asperiores error consequatur
+            reprehenderit quisquam quas, dolor laboriosam rem, nisi, ut ipsam
+            eius aspernatur voluptas accusantium quasi? Dolores.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab
+            delectus sapiente facilis. Asperiores error consequatur
+            reprehenderit quisquam quas, dolor laboriosam rem, nisi, ut ipsam
+            eius aspernatur voluptas accusantium quasi? Dolores.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePage
+export default SinglePage;
---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/885021/pexels-photo-885021.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/905485/pexels-photo-905485.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/1207918/pexels-photo-1207918.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/1566308/pexels-photo-1566308.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div>
+      <div className="h-[500px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className=" flex justify-between mt-8 gap-4">
+        {images.map((img, i) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+            key={img.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
