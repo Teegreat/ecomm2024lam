0d51d70ea61a5899900151e6d7a4aad1ddf0a13b
TeeGreat <toyinatofarati64@gmail.com>
2024-07-07 13:36:13 +0100

    authentication implemented

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -46,11 +46,16 @@ const Add = ({
               +
             </button>
           </div>
-          <div className="text-sm">
-            Only <span className="text-orange-500"> {stockNumber} item(s) </span>{" "}
-            left! <br />
-            {"Don't"} miss it
-          </div>
+          {stockNumber < 1 ? (
+            <div className="text-xs">Product is out of stock</div>
+          ) : (
+            <div className="text-xs">
+              Only{" "}
+              <span className="text-orange-500"> {stockNumber} item(s) </span>{" "}
+              left! <br />
+              {"Don't"} miss it
+            </div>
+          )}
         </div>
         <button className="w-36 text-sm rounded-3xl ring-1 ring-tee text-tee py-2 px-4 hover:bg-tee hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
           Add to Cart
